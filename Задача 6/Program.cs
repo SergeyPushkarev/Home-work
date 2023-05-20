1793330a3407033e12c9fc13e3e0c445da4e9e0e
Sergey <samtriezo@gmail.com>
2023-05-20 21:05:36 +0700

    сделал задачу 6

--- Задача 6/Program.cs
+++ Задача 6/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+    System.Console.WriteLine("Введенное число четное");
+else
+    System.Console.WriteLine("Введенное число нечетное");
